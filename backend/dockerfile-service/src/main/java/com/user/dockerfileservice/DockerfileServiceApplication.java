83c14437f6bfa858322b4a1fe6c682c15b1844bf
tr_mkhmiri <tr_mkhmiri@company.local>
2026-05-02 13:04:04 +0100

    fix: add scanBasePackages to include platform services and strategies

--- backend/dockerfile-service/src/main/java/com/user/dockerfileservice/DockerfileServiceApplication.java
+++ backend/dockerfile-service/src/main/java/com/user/dockerfileservice/DockerfileServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.user.dockerfileservice", "com.platform.dockerfileservice"})
 public class DockerfileServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(DockerfileServiceApplication.class, args);
